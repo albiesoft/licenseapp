d67b3310e6eb92325cda0b4bb5241111f216d339
Albert F <albert@albiesoft.com>
2018-12-28 10:57:31 +0000

    added heroku port

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const startServer = async () => {
   
   server.applyMiddleware({ app }); // app is from an existing express app
   
-  app.listen({ port: 4000 }, () =>
+  app.listen({ port: process.env.PORT || 4000 }, () =>
     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
   )
 };
