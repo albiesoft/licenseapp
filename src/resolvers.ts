6f13222daf97d89ecc388b047d19cec0f2ad98ee
Albert F <albert@albiesoft.com>
2018-12-28 13:51:37 +0000

    fixing the license checker

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -121,8 +121,9 @@ export const resolvers: IResolvers = {
         lc.save();
       }
 
-      const valid = await argon2.verify(lc.license, license);
-
+      const uuidV4Regex = /^[A-F\d]{8}-[A-F\d]{4}-4[A-F\d]{3}-[89AB][A-F\d]{3}-[A-F\d]{12}$/i;
+      const valid = uuidV4Regex.test(license);
+      
       if (!valid) {
 
         signNuller();
