7f04166cf9b14cb28367f5c92af092c6192198d5
Albert F <albert@albiesoft.com>
2018-12-28 11:39:09 +0000

    database

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,27 @@ const startServer = async () => {
     })
   );
 
-  await createConnection();
+  await createConnection({
+    url: 'postgres://wsbfdrgewkyloc:953a2f573166cdb11d2b1eb5274df4b89f37603342f88515a7720d514345cbda@ec2-54-247-74-131.eu-west-1.compute.amazonaws.com:5432/df7jcffv482igb',
+    type: 'postgres',
+    extra: {
+      ssl: true,
+    },
+    entities: [
+      "src/entity/**/*.ts"
+    ],
+    migrations: [
+      "src/migration/**/*.ts"
+    ],
+    subscribers: [
+      "src/subscriber/**/*.ts"
+    ],
+    cli: {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+    }
+  });
   
   server.applyMiddleware({ app }); // app is from an existing express app
   
